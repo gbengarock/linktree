9b7894f7bfbbfcfbb2de70c2f7c806c061cc8a53
gbengarock <temidayoolugbenga@yahoo.com>
2022-10-28 23:57:32 +0100

    add title attr

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = (props) => {
   return (
     <div className='w-full my-6 mx-auto flex items-center justify-center bg-[#EAECF0] hover:bg-[#D0D5DD] rounded-lg p-2 font-semibold'>
-        <a className='p-2' href={props.url}>{props.description}</a>
+        <a className='p-2' href={props.url} title={props.title}>{props.description}</a>
     </div>
   )
 }
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -5,11 +5,11 @@ const Links = () => {
   return (
     <div className='w-[80%] mx-auto flex-col py-2 mt-[-30px]'>
                 
-        <Button id='btn__zuri' url='https://training.zuri.team/' description='Zuri Team' />
-        <Button id='books' url='http://books.zuri.team' description='Zuri Books' />
-        <Button id='book__python' url='https://books.zuri.team/python-for-beginners?ref_id=Capo' description='Python Books' />
-        <Button id='pitch' url='https://background.zuri.team' description='Background Check for Coders' />
-        <Button id='book__design' url='https://books.zuri.team/design-rules' description='Design Books' />
+        <Button id='btn__zuri' url='https://training.zuri.team/' description='Zuri Team' title='' />
+        <Button id='books' url='http://books.zuri.team' description='Zuri Books' title='This is where you find books about design and coding' />
+        <Button id='book__python' url='https://books.zuri.team/python-for-beginners?ref_id=Capo' description='Python Books' title='Learn python in all its awesomeness at a pocket friendly price' />
+        <Button id='pitch' url='https://background.zuri.team' description='Background Check for Coders' title='Everything you need to know about that developer is right here' />
+        <Button id='book__design' url='https://books.zuri.team/design-rules' description='Design Books' title='Design in the 21st century' />
 
     </div>
   )
