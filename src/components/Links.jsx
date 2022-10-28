4bb9732d8be3cbff0d1e54514daaa5c93143a81a
Apo Olugbenga <99582201+gbengarock@users.noreply.github.com>
2022-10-28 14:32:40 +0100

    Update Links.jsx

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -7,7 +7,7 @@ const Links = () => {
                 
         <Button id='btn_zuri' url='https://training.zuri.team/' description='Zuri Team' />
         <Button id='books' url='http://books.zuri.team' description='Zuri Books' />
-        <Button id='book_python' url='https://books.zuri.team/python-for-beginners?ref_id=capo' description='Python Books' />
+        <Button id='book_python' url='https://books.zuri.team/python-for-beginners?ref_id=Capo' description='Python Books' />
         <Button id='pitch' url='https://background.zuri.team' description='Background Check for Coders' />
         <Button id='book_design' url='https://books.zuri.team/design-rules' description='Design Books' />
 
