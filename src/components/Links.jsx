c9f0d4be9665e4267d6e5494defe22a892095a1d
Apo Olugbenga <99582201+gbengarock@users.noreply.github.com>
2022-10-28 14:36:35 +0100

    edit ids

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -5,11 +5,11 @@ const Links = () => {
   return (
     <div className='w-[80%] mx-auto flex-col py-2 mt-[-30px]'>
                 
-        <Button id='btn_zuri' url='https://training.zuri.team/' description='Zuri Team' />
+        <Button id='btn__zuri' url='https://training.zuri.team/' description='Zuri Team' />
         <Button id='books' url='http://books.zuri.team' description='Zuri Books' />
-        <Button id='book_python' url='https://books.zuri.team/python-for-beginners?ref_id=Capo' description='Python Books' />
+        <Button id='book__python' url='https://books.zuri.team/python-for-beginners?ref_id=Capo' description='Python Books' />
         <Button id='pitch' url='https://background.zuri.team' description='Background Check for Coders' />
-        <Button id='book_design' url='https://books.zuri.team/design-rules' description='Design Books' />
+        <Button id='book__design' url='https://books.zuri.team/design-rules' description='Design Books' />
 
     </div>
   )
