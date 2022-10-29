ee9dd7f54af4334d40991c5eb251e3dcdb76fa2a
gbengarock <temidayoolugbenga@yahoo.com>
2022-10-29 12:00:50 +0100

    add share link hover effect

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,17 +4,18 @@ import avatarIcon from '../assets/Icon.png'
 import shareWeb from '../assets/shareWeb.png'
 import shareMobile from '../assets/shareMobile.png'
 import Button from './Button';
+import tooltip from '../assets/Tooltip.png'
 
 function Profile() {
-  const [display, setDisplay] = useState('none')
+  // const [display, setDisplay] = useState('none')
 
-  const handleShareLink = () => {
-    if ( display === 'none' ) {
-      setDisplay( 'block' )
-      } else {
-        setDisplay( 'none' )
-      }
-    }
+  // const handleShareLink = () => {
+  //   if ( display === 'none' ) {
+  //     setDisplay( 'block' )
+  //     } else {
+  //       setDisplay( 'none' )
+  //     }
+  //   }
 
   return (
     
@@ -22,11 +23,12 @@ function Profile() {
       
       <div className='w-full sm:w-[80%] mx-auto items-center mt-10'>
         {/* share link icon */}
-        <div className='flex items-end absolute right-10 top-3 sm:right-[500px]'>
-          <img onClick={handleShareLink} src={shareWeb} alt='/' className='hidden sm:flex' />
-          <img onClick={handleShareLink} src={shareMobile} alt='/' className='flex sm:hidden' />
-          <div className='w-[100px] bg-black text-white p-2 rounded-3xl absolute right-10' style={{display:display}}>Share Link</div>
-        </div>
+        <div className='flex items-end group absolute right-10 top-3 sm:right-[500px]'>
+          <img src={tooltip} alt='/' className='opacity-0 group-hover:opacity-100 relative top-5 left-3' />
+          <img src={shareWeb} alt='/' className='hidden sm:flex' />
+          <img src={shareMobile} alt='/' className='flex sm:hidden' />
+          {/* <div className='w-[100px] bg-black text-white p-2 rounded-3xl absolute right-10' style={{display:display}}>Share Link</div> */}
+          </div>
 
         {/* avatar section */}
 
@@ -39,7 +41,7 @@ function Profile() {
               </div>
             </div>
             
-            <h1 id='twitter' className='text-center font-bold text-2xl sm:text-4xl'>Temidayo Olugbenga</h1>
+            <h1 id='twitter' className='text-center font-bold text-2xl sm:text-4xl'>@temidayo_capo</h1>
             <h1 id='slack' className='hidden text-center font-bold text-2xl sm:text-4xl'>Capo</h1>
           
           </div>
@@ -47,13 +49,9 @@ function Profile() {
                 
       </div>
       <div className='w-[80%] mx-auto'>
-        <Button id='twitter' url='https://twitter.com/temidayo_capo' description='Twitter Link' />
+        <Button id='' url='https://twitter.com/temidayo_capo' description='Twitter Link' />
       </div>
-      <div className='hidden'>
-        <Button id='slack' url='https://app.slack.com/client/T042F7V19Q8/D047W3C6M2T/rimeto_profile/U048AKKMEFL' description='Slack Link' />
-      </div>
-      
-      
+           
     </div>
   );
 }
