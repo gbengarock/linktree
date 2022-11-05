bf4c30cf514ad542b437f3f8cc474a6ebef25d61
gbengarock <temidayoolugbenga@yahoo.com>
2022-11-05 10:23:41 +0100

    feat: add validation

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ContactForm = () => {
     
    const name = 'Capo'
-   
+
   return (
     <div className='w-full sm:py-24'>
         <div className='sm:w-[40%] mx-auto my-auto p-8'>
@@ -15,24 +15,20 @@ const ContactForm = () => {
             <div className='sm:flex my-2'>
                 <div className='flex flex-col sm:mr-10'>
                     <label className='text-xs text-gray-700 sm:text-sm font-medium my-2'>First name</label>
-                    <input name='first_name' id='first_name' type='text' placeholder='Enter your first name' className='py-2 px-3 rounded-lg items-center border focus:outline-[#B4CAFF] border-gray-300 shadow-sm gap-2 sm:w-[340px] sm:h-[44px] text-sm' />
-                    {/* <p className='text-[red] text-xs my-1'>{formErrors.first_name}</p> */}
+                    <input name='first_name' id='first_name' type='text' placeholder='Enter your first name' className='py-2 px-3 rounded-lg items-center border focus:outline-[#B4CAFF] border-gray-300 shadow-sm gap-2 sm:w-[340px] sm:h-[44px] text-sm' required />
                 </div>
                 <div className='flex flex-col'>
                     <label className='text-xs text-gray-700 sm:text-sm font-medium my-2'>Last name</label>
-                    <input name='last_name' id='last_name' type='text' placeholder='Enter your last name' className='py-2 px-3 rounded-lg items-center border focus:outline-[#B4CAFF] border-gray-300 shadow-sm gap-2 sm:w-[340px] sm:h-[44px] text-sm' />
-                    {/* <p className='text-[red] text-xs my-1'>{formErrors.last_name}</p> */}
+                    <input  name='last_name' id='last_name' type='text' placeholder='Enter your last name' className='py-2 px-3 rounded-lg items-center border focus:outline-[#B4CAFF] border-gray-300 shadow-sm gap-2 sm:w-[340px] sm:h-[44px] text-sm' required/>
                 </div>
             </div>
             <div className='flex flex-col my-2'>
                 <label className='text-xs text-gray-700 sm:text-base font-medium my-2'>Email</label>
-                <input name='email' id='email' type='email' placeholder='yourname@email.com' className='p-2 border focus:outline-[#B4CAFF] rounded-lg border-gray-300 shadow-sm gap-2 py-2 px-3 items-center text-sm sm:w-[720px] sm:h-[44px]' />
-                {/* <p className='text-[red] text-xs my-1'>{formErrors.email}</p> */}
+                <input name='email' id='email' type='email' placeholder='yourname@email.com' className='p-2 border focus:outline-[#B4CAFF] rounded-lg border-gray-300 shadow-sm gap-2 py-2 px-3 items-center text-sm sm:w-[720px] sm:h-[44px]' required/>
             </div>
             <div className='flex flex-col my-2'>
                 <label className='text-xs text-gray-700 sm:text-base font-medium my-2'>Message</label>
-                <textarea name='message' id='message'className='border focus:outline-[#B4CAFF] rounded-lg border-gray-300 shadow-sm gap-2 py-2 px-3 items-center text-sm sm:w-[720px]' rows='6' placeholder="Send me a message and I'll reply you as soon as possible..." />
-                {/* <p className='text-[red] text-xs my-1'>{formErrors.message}</p> */}
+                <textarea name='message' id='message'className='border focus:outline-[#B4CAFF] rounded-lg border-gray-300 shadow-sm gap-2 py-2 px-3 items-center text-sm sm:w-[720px]' rows='6' placeholder="Send me a message and I'll reply you as soon as possible..." required/>
             </div>
 
             <p className='text-gray-600 my-4'><input type='checkbox' className='mr-3 cursor-pointer' />You agree to providing your data to {name} who may contact you.</p>     
